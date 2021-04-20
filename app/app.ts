17435e6cc0676c1280c02b4effd1aef4d170ca0e
Your Name <you@example.com>
2021-04-21 03:08:25 +0500

    dataTypes

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 function startGame() {
     // starting a new game
+
+    let playerName: string = "Hamza";
+    logPlayer(playerName);
+
     var messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
-  }
-  
-  document.getElementById('startGame')!.addEventListener('click', startGame);
+    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+
+function logPlayer(name) {
+    console.log(`New Game Starting Player: ${name}`)
+}
+
+document.getElementById('startGame')!.addEventListener('click', startGame);
