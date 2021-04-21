ec6f8d2bf4e45df607d3cacf9358c35f3f58e4bc
Your Name <you@example.com>
2021-04-21 18:05:58 +0500

    typescript functions

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,33 @@
 function startGame() {
     // starting a new game
 
-    let playerName: string = "Hamza";
+    let playerName: string | undefined = getInputValue("playername");
     logPlayer(playerName);
 
-    var messagesElement = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    postScore(80, playerName)
 }
 
-function logPlayer(name) {
+function logPlayer(name: string = "MultiMath Player"): void {
     console.log(`New Game Starting Player: ${name}`)
 }
 
+function getInputValue(elementId: string): string | undefined {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+    if (inputElement.value === "") {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName: string = "MultiMath Player"): void {
+    const scoreElement: HTMLElement | null = document.getElementById("postedScore");
+    scoreElement!.innerText = `${score} - ${playerName}`
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessege = (messege: string) => console.log(messege);
+
+logMessege("Welcome")
